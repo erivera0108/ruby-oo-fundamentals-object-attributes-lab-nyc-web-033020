08845a71d7dbed25b015520ddbf2c61de7f06ae9
Edgar <edgar.a.rivera08@gmail.com>
2020-03-31 14:49:10 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,23 @@
 class Person
-    attr_accessor :name, :job
-    def initialize
+    # attr_accessor :name, :job
+    # def initialize
+    #     @name = name
+    #     @job = job
+    # end
+
+    def name=(name)
         @name = name
+    end
+
+    def name 
+        @name
+    end
+
+    def job=(job)
         @job = job
     end
 
+    def job 
+        @job
+    end
 end
